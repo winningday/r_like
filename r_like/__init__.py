f5d0ff431d974870f45b832f2fb5c6a86150d2c3
winningday <137007298+winningday@users.noreply.github.com>
2024-06-06 21:49:32 -0700

    Update __init__.py to include all functions in order of appearance

---
+++ r_like/__init__.py
@@ -0,0 +1,4 @@
+# r_like/__init__.py
+from .r_like import choose, seq, mean, median, sd, var, sum_values, prod, summary, fractions, factorial, plot
+
+__all__ = ['choose', 'seq', 'mean', 'median', 'sd', 'var', 'sum_values', 'prod', 'summary', 'fractions', 'factorial', 'plot']
